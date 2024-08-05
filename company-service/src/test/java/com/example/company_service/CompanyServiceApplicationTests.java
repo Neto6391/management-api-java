3c613f41dc936337910a4a6144ea2bbdb5c8be6b
Neto6391 <neto6391@gmail.com>
2024-08-05 14:16:59 -0300

    feat: remove for while test in CompanyServiceApplication

--- company-service/src/test/java/com/example/company_service/CompanyServiceApplicationTests.java
+++ company-service/src/test/java/com/example/company_service/CompanyServiceApplicationTests.java
@@ -1,12 +1,10 @@
 package com.example.company_service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CompanyServiceApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
