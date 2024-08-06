368a27ce7fb18cc8959c82a78ac66b798b998d2b
Neto6391 <neto6391@gmail.com>
2024-08-06 18:24:27 -0300

    fix: refactor mappers

--- company-service/src/main/java/com/example/company_service/mapper/CompanyMapper.java
+++ company-service/src/main/java/com/example/company_service/mapper/CompanyMapper.java
@@ -1,13 +1,17 @@
 package com.example.company_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 import com.example.company_service.dto.CompanyDto;
 import com.example.company_service.entity.Company;
 
 @Mapper(componentModel = "spring")
-public interface CompanyMapper {
-    CompanyDto toDto(Company company);
+public abstract class CompanyMapper {
 
-    Company toEntity(CompanyDto companyDto);
+    public static final CompanyMapper INSTANCE = Mappers.getMapper(CompanyMapper.class);
+
+    public abstract CompanyDto toDto(Company company);
+
+    public abstract Company toEntity(CompanyDto companyDto);
 }
