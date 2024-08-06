e7e6e28ac3a595adb1d16a6190a6d892c1e2b3c7
Neto6391 <neto6391@gmail.com>
2024-08-06 18:24:10 -0300

    fix: refactor Spring start up application to scan components by package

--- company-service/src/main/java/com/example/company_service/CompanyServiceApplication.java
+++ company-service/src/main/java/com/example/company_service/CompanyServiceApplication.java
@@ -5,12 +5,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import io.github.cdimascio.dotenv.DotenvEntry;
 
+@ComponentScan("com.example.company_service")
 @SpringBootApplication
 public class CompanyServiceApplication {
 
